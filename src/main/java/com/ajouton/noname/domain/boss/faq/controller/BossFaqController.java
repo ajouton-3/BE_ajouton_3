4a161c62f9113b70d23fe11c8fc54da691024168
Yoo Jin <dbwls30131@ajou.ac.kr>
2023-10-29 05:27:07 +0900

    <FEAT> 특정 동아리의 FAQ 전체 목록 가져오기 API 개발

--- src/main/java/com/ajouton/noname/domain/boss/faq/controller/BossFaqController.java
+++ src/main/java/com/ajouton/noname/domain/boss/faq/controller/BossFaqController.java
@@ -1,12 +1,15 @@
 package com.ajouton.noname.domain.boss.faq.controller;
 
+import com.ajouton.noname.domain.boss.faq.dto.FaqDto;
 import com.ajouton.noname.domain.boss.faq.dto.PostFaqDto;
 import com.ajouton.noname.domain.club.service.ClubService;
 import com.ajouton.noname.domain.faq.service.FaqService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,12 @@ public class BossFaqController {
     clubService.isValidClub(clubId);
     faqService.postFaq(clubId, postFaqDto);
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping("")
+  public ResponseEntity<List<FaqDto>> getClubFaqList(@RequestParam("clubId") Long clubId) {
+    clubService.isValidClub(clubId);
+    List<FaqDto> faqList = faqService.getClubFaqList(clubId);
+    return ResponseEntity.status(HttpStatus.OK).body(faqList);
   }
 }
--- src/main/java/com/ajouton/noname/domain/faq/service/FaqService.java
+++ src/main/java/com/ajouton/noname/domain/faq/service/FaqService.java
@@ -2,12 +2,15 @@ package com.ajouton.noname.domain.faq.service;
 
 import com.ajouton.noname.domain.activity.dto.ActivityListResponse;
 import com.ajouton.noname.domain.activity.entity.Activity;
+import com.ajouton.noname.domain.boss.activity.dto.ActivityDto;
+import com.ajouton.noname.domain.boss.faq.dto.FaqDto;
 import com.ajouton.noname.domain.boss.faq.dto.PostFaqDto;
 import com.ajouton.noname.domain.club.entity.Club;
 import com.ajouton.noname.domain.club.repository.ClubRepository;
 import com.ajouton.noname.domain.faq.dto.FaqListResponse;
 import com.ajouton.noname.domain.faq.entity.Faq;
 import com.ajouton.noname.domain.faq.repository.FaqRepository;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -47,5 +50,14 @@ public class FaqService {
             .answer(postFaqDto.getAnswer())
             .club(club).build();
         faqRepository.save(faq);
+    }
+
+    public List<FaqDto> getClubFaqList(Long clubId) {
+        Club club = clubRepository.findById(clubId).get();
+        List<Faq> faqList = faqRepository.findAllByClub(club);
+        List<FaqDto> clubFaqResult = faqList.stream()
+            .map(faq -> new FaqDto(faq))
+            .collect(Collectors.toList());
+        return clubFaqResult;
     }
 }
